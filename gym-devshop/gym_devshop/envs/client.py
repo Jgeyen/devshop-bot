a32711fe2fc785a0bf97b81e06f34d723c90a45e
jgeyen <jason.geyen@optelgroup.com>
2019-11-19 20:16:44 -0600

    Working on api client

--- gym-devshop/gym_devshop/envs/client.py
+++ gym-devshop/gym_devshop/envs/client.py
@@ -1,4 +1,23 @@
 import json
 import requests
+from collections import namedtuple
 
-api_url_base = 'https://api.digitalocean.com/v2/'
+api_url_base = 'https://localhost:5001/api/'
+
+URL_STATE = "state"
+MAX_NUM_SHARES = 2147483647
+MAX_SHARE_PRICE = 5000
+MAX_OPEN_POSITIONS = 5
+MAX_STEPS = 20000
+
+class DevshopClient
+    def getState
+        requestUrl = api_url_base + URL_STATE
+        response = requests.get(requestUrl, verify=False)
+        print(response.json())
+        myobj = response.json()
+
+        x = json.loads(response.content, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+
+        print(x.bank)
+        print(myobj)
